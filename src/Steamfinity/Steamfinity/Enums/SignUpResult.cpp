c30766599898f308eece3a30a2a8279ba8ce3c36
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-23 16:43:27 +0200

    Add SignUpResult Enum

---
+++ src/Steamfinity/Steamfinity/Enums/SignUpResult.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "SignUpResult.h"
+#if __has_include("SignUpResult.g.cpp")
+#include "SignUpResult.g.cpp"
+#endif
+
+namespace winrt::Steamfinity::implementation
+{
+    int32_t SignUpResult::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void SignUpResult::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ src/Steamfinity/Steamfinity/Enums/SignUpResult.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "SignUpResult.g.h"
+
+namespace winrt::Steamfinity::implementation
+{
+    struct SignUpResult : SignUpResultT<SignUpResult>
+    {
+        SignUpResult() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Steamfinity::factory_implementation
+{
+    struct SignUpResult : SignUpResultT<SignUpResult, implementation::SignUpResult>
+    {
+    };
+}
