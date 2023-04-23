48b21ac5e0878384518c48a1368a029d2c31805a
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-23 16:58:52 +0200

    Add RefreshTokenDetails Model

---
+++ src/Steamfinity/Steamfinity/Models/RefreshTokenDetails.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+
+#include "Models/RefreshTokenDetails.h"
+#include "Models.RefreshTokenDetails.g.cpp"
+
+using namespace winrt;
+
+using namespace Windows::Foundation;
+using namespace Steamfinity::Enums;
+
+namespace winrt::Steamfinity::Models::implementation
+{
+	RefreshTokenDetails::RefreshTokenDetails(Steamfinity::Enums::SignUpResult const& signUpResult, hstring const& token, DateTime const& expirationTime) noexcept :
+		m_signUpResult{ signUpResult },
+		m_token{ token },
+		m_expirationTime{ expirationTime }
+	{
+	}
+
+	SignUpResult RefreshTokenDetails::SignUpResult() const noexcept
+	{
+		return m_signUpResult;
+	}
+
+	hstring RefreshTokenDetails::Token() const noexcept
+	{
+		return m_token;
+	}
+
+	DateTime RefreshTokenDetails::ExpirationTime() const noexcept
+	{
+		return m_expirationTime;
+	}
+}
---
+++ src/Steamfinity/Steamfinity/Models/RefreshTokenDetails.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Models.RefreshTokenDetails.g.h"
+
+namespace winrt::Steamfinity::Models::implementation
+{
+	struct RefreshTokenDetails : RefreshTokenDetailsT<RefreshTokenDetails>
+	{
+		RefreshTokenDetails(Steamfinity::Enums::SignUpResult const&, hstring const&, Windows::Foundation::DateTime const&) noexcept;
+
+		Steamfinity::Enums::SignUpResult SignUpResult() const noexcept;
+		hstring Token() const noexcept;
+		Windows::Foundation::DateTime ExpirationTime() const noexcept;
+
+		private:
+
+		Steamfinity::Enums::SignUpResult m_signUpResult;
+		hstring m_token;
+		Windows::Foundation::DateTime m_expirationTime;
+	};
+}
+
+namespace winrt::Steamfinity::Models::factory_implementation
+{
+	struct RefreshTokenDetails : RefreshTokenDetailsT<RefreshTokenDetails, implementation::RefreshTokenDetails>
+	{
+	};
+}
